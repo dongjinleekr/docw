892c91f0432058ebd71bfdd893e2f9c483c1b77b
dongjinleekr <dongjin.lee.kr@gmail.com>
2014-08-25 03:47:05 +0900

    create_node arguments changed

--- sbin/create_node.sh
+++ sbin/create_node.sh
@@ -3,38 +3,22 @@
 set -e
 
 # help
-if [ $# -eq 9 ]
+if [ $# -eq 6 ]
 then
 	CLIENT_ID=$1
 	API_KEY=$2
 	HOSTNAME=$3
 	SIZE_ID=$4
-	IMAGE=$5
+	OS_IMAGE=$5
 	REGION=$6
-	EMAIL_SERVER=$7
-	EMAIL_USERNAME=$8
-	EMAIL_PASSWORD=$9
 else
-	echo "Usage: $0 {do-client-id} {do-api-key} {hostname} {do-size-id} {image(ubuntu-14.04)} {region(sgp1|nyc2)} {email-server} {email-account} {email-password}"
+	echo "Usage: $0 {do-client-id} {do-api-key} {hostname} {do-size-id} {os-image(ubuntu-14.04)} {region(sgp1|nyc2)}"
 	exit 1
 fi
 
 PYTHON=$(which python3)
-CURRENT_DIR=$(dirname $(readlink -f $0))
-ROOT_PASSWORD='root_password'
+SBIN_DIR=$(dirname $(readlink -f $0))
 
-# create node
-RESULT=$(${PYTHON} ${CURRENT_DIR}/create.py -c ${CLIENT_ID} -a ${API_KEY} -n ${HOSTNAME} -s ${SIZE_ID} -i ${IMAGE} -r ${REGION})
+. ${SBIN_DIR}/functions.sh
 
-TOKENS=( ${RESULT} ); 
-
-PUBLIC_IP=${TOKENS[0]}
-PRIVATE_IP=${TOKENS[1]}
-
-# acquire temporary password
-TMP_PASSWORD=$(${PYTHON} ${CURRENT_DIR}/fetchmail.py -s ${EMAIL_SERVER} -u ${EMAIL_USERNAME} -p ${EMAIL_PASSWORD} -n ${HOSTNAME})
-
-ssh-keygen -R ${PUBLIC_IP} -f "${HOME}/.ssh/known_hosts" >> /dev/null 2>&1
-
-# reset password
-${CURRENT_DIR}/init-${REGION}.ex ${PUBLIC_IP} ${TMP_PASSWORD} ${ROOT_PASSWORD} >> /dev/null 2>&1
+create_node $@
