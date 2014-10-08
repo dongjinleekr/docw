2690809edfef432eaeee74da076fb3c2845095b3
dongjinleekr <dongjin.lee.kr@gmail.com>
2014-10-08 17:36:48 +0900

    docw python package created: version 0.7

---
+++ utils/docw/__init__.py
@@ -0,0 +1,9 @@
+"""
+Copyright (c) 2014  Dongjin Lee <dongjin.lee.kr@gmail.com>
+
+This module offers droplet management for docw
+(https://github.com/dongjinleekr/docw)
+"""
+__author__ = "Dongjin Lee <dongjin.lee.kr@gmail.com>"
+__license__ = "Apache License, Version 2.0"
+__version__ = "0.7"
---
+++ utils/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import docw
+
+if sys.version_info[0] != 3:
+    print("Error: python 3 is required.")
+    exit()
+
+setup(
+    # Application name:
+    name='docwutils',
+
+    # Version number:
+    version=docw.__version__,
+
+    # Application author details:
+    author="Dongjin Lee",
+    author_email="dongjin.lee.kr@gmail.com",
+
+    # Packages
+    packages=["docw"],
+
+    # Details
+    url="https://github.com/dongjinleekr/docw",
+
+    #
+    license=docw.__license__,
+    description="python scripts for docw.",
+
+    long_description=
+"""\
+python scripts for docw, available in Python 3.0+.
+""",
+
+    # Dependent packages (distributions)
+    install_requires=[
+        'psutil >= 2.1.3',
+        'dopy >= 0.2.5',
+    ],
+)
