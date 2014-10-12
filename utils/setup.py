c90abecb3083c59ab2e13a759406495d600789f0
dongjinleekr <dongjin.lee.kr@gmail.com>
2014-10-12 22:33:45 +0900

    setup.py realigned. dependency on lxml added

--- utils/setup.py
+++ utils/setup.py
@@ -3,51 +3,53 @@
 import sys
 
 try:
-    from setuptools import setup
+  from setuptools import setup
 except ImportError:
-    from distutils.core import setup
+  from distutils.core import setup
 
 import docw
 
 if sys.version_info[0] != 3:
-    print("Error: python 3 is required.")
-    exit()
+  print("Error: python 3 is required.")
+  exit()
 
 setup(
-    # Application name:
-    name='docwutils',
+  # Application name:
+  name='docwutils',
 
-    # Version number:
-    version=docw.__version__,
+  # Version number:
+  version=docw.__version__,
 
-    # Application author details:
-    author="Dongjin Lee",
-    author_email="dongjin.lee.kr@gmail.com",
+  # Application author details:
+  author="Dongjin Lee",
+  author_email="dongjin.lee.kr@gmail.com",
 
-    # Packages
-    packages=["docw"],
+  # Packages
+  packages=["docw"],
 
-    # Details
-    url="https://github.com/dongjinleekr/docw",
+  # Details
+  url="https://github.com/dongjinleekr/docw",
 
-    #
-    license=docw.__license__,
-    description="python scripts for docw.",
+  #
+  license=docw.__license__,
+  description="python scripts for docw.",
 
-    long_description=
+  long_description=
 """\
 python scripts for docw, available in Python 3.0+.
 """,
 
-    # Dependent packages (distributions)
-    install_requires=[
-        'psutil >= 2.1.3',
-        'dopy >= 0.2.5',
+  # Dependent packages (distributions)
+  install_requires=[
+    'psutil >= 2.1.3',
+    'dopy >= 0.2.5',
+    'lxml >= 3.3.3',
+  ],
+  
+  entry_points={
+    'console_scripts': [
+      'docw-gettmp = docw.gettmp:main',
+      'docw-hdconf = docw.hdconf:main',
     ],
-    
-    entry_points={
-        'console_scripts': [
-            'docw-gettmp = docw.gettmp:main',
-        ],
-    },
+  },
 )
