fc2be79c8b58055fd403cb4719110c5cef266f8d
dongjinleekr <dongjin.lee.kr@gmail.com>
2015-01-12 16:43:23 +0900

    digitalocean api wrapper (v2) added: copied from https://github.com/koalalorenzo/python-digitalocean/tree/1.2

---
+++ utils/digitalocean/Metadata.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+import requests
+from urllib.parse import urljoin
+
+from .baseapi import BaseAPI
+
+
+class Metadata(BaseAPI):
+    """
+        Metadata API: Provide useful information about the current Droplet.
+        See: https://developers.digitalocean.com/metadata/#introduction
+    """
+    droplet_id = None
+    end_point = "http://169.254.169.254/metadata/v1"
+
+    def __init__(self, *args, **kwargs):
+        super(Metadata, self).__init__(*args, **kwargs)
+        self.end_point = "http://169.254.169.254/metadata/v1"
+
+    def get_data(self, url, headers=dict(), params=dict(), render_json=True):
+        """
+            Customized version of get_data to directly get the data without
+            using the authentication method.
+        """
+        if "https" not in url:
+            url = urljoin(self.end_point, url)
+
+        response = requests.get(url, headers=headers, params=params)
+
+        if render_json:
+            return response.json()
+        return response.content
+
+    def load(self):
+        metadata = self.get_data("v1.json")
+
+        for (k, v) in metadata.items():
+            setattr(self, k, v)
+
+        return self
+
+    def __str__(self):
+        return "%s" % self.droplet_id
